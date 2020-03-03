53a75b07b7482ef92ff04fd05c6bb6fc46ce912a
vatsa03 <vatsa.kushagra@gmail.com>
2020-03-03 17:43:47 +0530

    Update diskSize.py
    updated

--- diskSize.py
+++ diskSize.py
@@ -3,7 +3,7 @@ fp = open("C:/Users/kvatsa/Desktop/git_hub/size.txt",'r+')
 sz= fp.read()
 sz= list(sz.split("\n"))
 sz= sz[-6]
-sz= sz.split()
+sz= sz.split(" ")
 sz= int(sz[1])
 sz= sz/(1e+6)
 print(sz)
