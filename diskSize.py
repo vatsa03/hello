dc074769c31a68484e5e56cc83a60b5ca051a3d3
kush <vatsa.kushagra@gmail.com>
2020-03-03 22:49:40 -0800

    updated diskSize

--- diskSize.py
+++ diskSize.py
@@ -1,10 +1,16 @@
-fp = open("C:/Users/kvatsa/Desktop/git_hub/size.txt",'r+')
-
+fp = open("C:/Users/kushagra/Desktop/git_hub/size.txt",'r+')
 sz= fp.read()
 sz= list(sz.split("\n"))
 sz= sz[-6]
-sz= sz.split(" ")
-sz= int(sz[1])
+sz= sz.split("KB available on disk.")
+sz=sz[0]
+sz=sz.strip(" ")
+sz= int(sz)
 sz= sz/(1e+6)
 print("available disk size ",sz)
+
+
+
+
+
 
